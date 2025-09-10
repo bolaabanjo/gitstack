9c1b6aa371a15f07ac4705b22477a83c948da631
bolaabanjo <royadebanjo@gmail.com>
2025-09-10 07:39:47 +0100

    debugging

--- cli.py
+++ cli.py
@@ -1,1 +1,12 @@
 import click 
+
+@click.command()
+#@click.option('--name', '-n', default='Bola', help='Input your First Name')
+
+@click.option('--name', '-n', default='Bola', help='Input your First Name')
+
+def main(name):
+    click.echo("Hello, World, My name is {}".format(name))
+
+if __name__ == '__main__':
+    main()
