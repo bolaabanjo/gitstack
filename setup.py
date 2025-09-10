439ddb49e8cf61aab284fd4931010f0eb7007690
bolaabanjo <royadebanjo@gmail.com>
2025-09-10 21:10:18 +0100

    Published package ready to install

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+
 setup(
     name="gitstack",
-    version="0.1.0",
+    version="0.2.0",
     description="An advanced modern version control system",
     packages=find_packages(),
     authors=["Bola Banjo <omogbolahanng@gmail.com>"],
@@ -11,5 +14,11 @@ setup(
         "click",
         "tabulate",
     ],
-    entry_points={"console_scripts": ["gitstack=gitstack.main:main"]},
+    entry_points={
+        "console_scripts":[
+        "gitstack=gitstack.main:main"
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
