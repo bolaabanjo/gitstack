5903a79937083e454dacfea9fcb428e1454eb265
bolaabanjo <royadebanjo@gmail.com>
2025-09-26 04:53:58 +0100

    updated app/login auth

---
+++ app/login/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { LoginForm } from "@/components/login-form";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function LoginPage() {
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+ useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  return (
+    <div className="relative bg-background min-h-svh flex flex-col items-center justify-center p-6 md:p-10">
+      {/* Mode toggle at top right */}
+      <div className="absolute top-6 right-6 z-10">
+        <ModeToggle />
+      </div>
+      {/* Login form */}
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
