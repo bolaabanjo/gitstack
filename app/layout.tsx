724de43fdfdbb86b90212df08b9e24f6b3c9ca55
bolaabanjo <royadebanjo@gmail.com>
2025-09-27 00:23:43 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider 
+    <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     signInUrl="/login" 
     signUpUrl="/register"
     afterSignInUrl="/dashboard"
