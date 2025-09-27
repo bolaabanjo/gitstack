fb1444042415d3a1f4eb4ebbfade6e07c5e09b04
bolaabanjo <royadebanjo@gmail.com>
2025-09-27 02:28:29 +0100

    added links to register page

--- components/registration-form.tsx
+++ components/registration-form.tsx
@@ -241,8 +241,8 @@ export function RegistrationForm({ className, ...props }: React.ComponentProps<"
         </form>
       )}
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4 mt-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our <a href="terms">Terms of Service</a>{" "}
+        and <a href="/privacy">Privacy Policy</a>.
       </div>
     </div>
   );
