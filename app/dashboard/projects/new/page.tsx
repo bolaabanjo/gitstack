58a1ccffc4e09d553e5f70053e0cf1c4bf68f741
bolaabanjo <royadebanjo@gmail.com>
2025-10-04 19:10:18 +0100

    updated empty state

--- app/dashboard/projects/new/page.tsx
+++ app/dashboard/projects/new/page.tsx
@@ -79,7 +79,7 @@ export default function CreateNewProjectPage() {
   return (
     <div className="container mx-auto p-4 md:p-6 lg:p-8 max-w-2xl">
       <div className="flex items-center justify-between mb-8">
-        <h1 className="text-3xl font-bold tracking-tight">Create a New Stack</h1>
+        <div><h1 className="text-3xl font-bold tracking-tight">Create a New Stack</h1></div>
         <p>Stacks are just repos that contain a project&apos;s files and version history.</p>
         <Link href="/dashboard/projects" passHref>
           <Button variant="outline" className='rounded-full cursor-pointer'>Cancel</Button>
