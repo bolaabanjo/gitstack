75d98dded9a26505c9a473be9d60b94e514bf054
bolaabanjo <royadebanjo@gmail.com>
2025-10-07 03:16:40 +0100

    .

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -77,12 +77,12 @@ export default function Topbar({ toggleSidebar, isSidebarCollapsed }: TopbarProp
 
       {/* Middle Section: Search Bar (Responsive) */}
       <div className="flex-1 max-w-sm mx-4">
-        <div className="relative rounded-full">
+        <div className="relative">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
           <Input
             type="search"
             placeholder="Search snapshots, files, deployments..."
-            className="w-full pl-9 h-9"
+            className="w-full pl-9 h-10 rounded-full"
           />
         </div>
       </div>
